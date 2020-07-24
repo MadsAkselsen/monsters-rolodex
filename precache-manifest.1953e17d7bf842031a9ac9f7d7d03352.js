303b781b81d01432cb909a0c9e1538f0a5d38844
Mads Akselsen <akselsenmads@gmail.com>
2020-07-24 14:35:53 +0200

    Updates

---
+++ precache-manifest.1953e17d7bf842031a9ac9f7d7d03352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc44e134797b8463511ed6cbc5f5c962",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e9a754badad067bb8271",
+    "url": "/monsters-rolodex/static/css/main.f219193e.chunk.css"
+  },
+  {
+    "revision": "bc4f0aec8753b31eee9d",
+    "url": "/monsters-rolodex/static/js/2.e601ea17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e601ea17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9a754badad067bb8271",
+    "url": "/monsters-rolodex/static/js/main.00adeda2.chunk.js"
+  },
+  {
+    "revision": "99aeed3ea4362ed85205",
+    "url": "/monsters-rolodex/static/js/runtime-main.dcaacdb6.js"
+  }
+]);
